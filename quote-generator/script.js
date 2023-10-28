2eb59687f3cb378f9b074d332cf99a6d5e611452
NadiaPia <206i417@gmail.com>
2023-10-27 23:30:25 -0700

    Add request API in script.js in quote-generator

---
+++ quote-generator/script.js
@@ -0,0 +1,26 @@
+let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    console.log('quote', quote)
+
+}
+
+
+// Get Quotes From API
+
+async function getQuotes() {    
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+     try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();        
+        newQuote()
+    } catch (error) {
+        console.log(error)
+
+    }
+ }
+// On Load
+
+getQuotes();
