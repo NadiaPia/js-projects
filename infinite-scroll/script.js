a05bc0609c73054175ab6c512e879ade569663b7
NadiaPia <206i417@gmail.com>
2023-11-07 23:32:23 -0800

    Add DOM elements in the script.js add functionality to show 10 pictires

--- infinite-scroll/script.js
+++ infinite-scroll/script.js
@@ -1,16 +1,58 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let photosArray = [];
+
 // Unsplash API
 
 const count = 10;
 const apiKey = SECRET;
 const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
-//Get pfotos from Unsplash API
+//Helper function to set Attributes on DOM Elements
 
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
+//Create Element for links and photos, add to DOM
+function displayPhotos() {
+    photosArray.forEach((photo) => {
+        //Create <a> to link to Unsplash
+        const item = document.createElement('a');
+        // item.setAttribute('href', photo.links.html);
+        // item.setAttribute('target', '_blank'); //it will be opent in a new tab
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        })
+
+        //Create <img> for photo
+        const img = document.createElement('img');
+        // img.setAttribute('src', photo.urls.regular);
+        // img.setAttribute('alt', photo.alt_description);
+        // img.setAttribute('title', photo.alt_description);
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description
+        })
+
+        //Put <img> inside <a>, then put both inside imageContainer Element
+        item.appendChild(img);   
+        imageContainer.appendChild(item);
+    });
+}
+
+//Get pfotos from Unsplash API
 async function getPhotos() {
     try {
         const response = await fetch(apiUrl);
-        const data = await response.json();
-        console.log("data", data)
+        photosArray = await response.json();
+        displayPhotos()
+        console.log("photosArray", photosArray)
     } catch (error) {
         //Catch Error Here
     }
