991a29508a8fbf615a61e21f5704efbce531beb5
NadiaPia <206i417@gmail.com>
2023-12-05 23:38:55 -0800

    Add DarkMode and lightMode functions

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -1,4 +1,38 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+
+//Dark Mode Styles
+ function darkMode() {
+    nav.style.backgroundColor = "rgb(0 0 0 / 50%)";    
+    textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
+    //console.log("toggleIcon.children", toggleIcon.children) //in html span + icon
+    toggleIcon.children[0].textContent = 'Dark Mode';  //span
+    toggleIcon.children[1].classList.remove('fas-sun');  //icon
+    toggleIcon.children[1].classList.add('fas-moon');  //icon
+    image1.src = 'img/undraw_proud_coder_dark.svg';
+    image2.src = 'img/undraw_feeling_proud_dark.svg';
+    image3.src = 'img/undraw_conceptual_idea_dark.svg';
+ }
+
+ 
+//Light Mode Styles
+function lightMode() {
+    nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
+    textBox.style.backgroundColor = "rgb(0 0 0 / 50%)"; 
+    toggleIcon.children[0].textContent = 'Light Mode';  //span
+    toggleIcon.children[1].classList.remove('fas-moon');  //icon
+    toggleIcon.children[1].classList.add('fas-sun');  //icon
+    image1.src = 'img/undraw_proud_coder_light.svg';
+    image2.src = 'img/undraw_feeling_proud_light.svg';
+    image3.src = 'img/undraw_conceptual_idea_light.svg';
+ }
+
 
 // Switch Theme Dynamically
 
@@ -6,8 +40,10 @@ function switchTheme(event) {
     console.log("event", event.target.checked);
     if (event.target.checked) {
         document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light');
+        lightMode();
     }
 }
 
