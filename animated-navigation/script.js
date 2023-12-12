329410703b1be3ba31301c3fea4736e355fd124c
NadiaPia <206i417@gmail.com>
2023-12-12 13:26:19 -0800

    Finish animated navigation project

--- animated-navigation/script.js
+++ animated-navigation/script.js
@@ -5,13 +5,13 @@ const nav2 = document.getElementById("nav-2");
 const nav3 = document.getElementById("nav-3");
 const nav4 = document.getElementById("nav-4");
 const nav5 = document.getElementById("nav-5");
-const navItems = [nav1, nav2, nav3,nav4,nav5]
+const navItems = [nav1, nav2, nav3, nav4, nav5];
 
 function navAnimation(direction1, direction2) {
     navItems.forEach((nav, i) => {
         nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`)
     })
-}
+};
 
 function toggleNav() {
     //Toggle: Menu Bars Open/Closed
@@ -23,7 +23,7 @@ function toggleNav() {
         //I can remove absent class from a classlist, but not to replace it as I need the old class in this case I add any class that will not affect on the functionality, f.e: "overlay-slide-left" to the <div class="overlay"> as it will not be visual 
         overlay.classList.replace('overlay-slide-left', 'overlay-slide-right');
         //Animate In - Nav Items
-        navAnimation('out', 'in');        
+        navAnimation('out', 'in');
     } else {
         overlay.classList.replace('overlay-slide-right', 'overlay-slide-left');
         //Animate Out - Nav Items   
